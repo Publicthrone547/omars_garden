2352f3a0b4291bbc5778436cb89cc84ffaeef4c4
publicthrone547 <publicthrone547@users.noreply.github.com>
2026-02-11 08:25:58 +0500

    Created some components

---
+++ app/components/sections/Hero.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="min-h-screen flex mx-auto items-center text-center justify-center">
+        <div class="text-6xl font-bold">
+            OmarX's Garden
+        </div>
+    </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,2 +1,10 @@
+<script setup lang="ts">
+import Hero from '~/components/sections/Hero.vue';
+
+</script>
+
 <template>
+    <div>
+        <Hero/>
+    </div>
 </template>
